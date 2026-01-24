e3a01a94ed66f1cb2b54f0ed0df770842ea88c76
parumen <thonglo02cocoa@gmail.com>
2026-01-24 19:19:24 +0900

    auto update 2026/01/24 19:19:24.81

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,21 @@
 @echo off
+setlocal enabledelayedexpansion
+
 echo --- 1. ビルドを開始します ---
 call npm run build
 
-echo --- 2. GitHubに送ります ---
+echo --- 2. GitHubへの送信準備 (assetsを強制追加) ---
+:: .gitignoreを無視してdistフォルダを無理やり追加します
+git add -f dist/
+
+:: その他の変更も追加
 git add .
-git commit -m "auto update %date% %time%"
+
+echo --- 3. コミットして送信します ---
+set datetime=%date% %time%
+git commit -m "auto update !datetime!"
 git push origin master
 
-echo --- 完了！ラズパイ側で update.sh を実行してください ---
+echo.
+echo --- 完了！GitHubに assets フォルダがあるか確認してください ---
 pause
