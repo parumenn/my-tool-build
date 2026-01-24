774ca8f61ff0bbed4e113fd520ee1b962db86b6b
parumen <thonglo02cocoa@gmail.com>
2026-01-24 19:24:43 +0900

    pre-rebase update 2026/01/24 19:24:43.39

--- deploy.bat
+++ deploy.bat
@@ -4,19 +4,20 @@ setlocal enabledelayedexpansion
 echo --- 1. ビルドを開始します ---
 call npm run build
 
-echo --- 2. GitHub側の変更（READMEなど）を取り込みます ---
-:: 先にリモートの変更を取得して合体させます
-git pull origin master --rebase
-
-echo --- 3. Gitの無視設定を上書きして追加します ---
+echo --- 2. 手元の変更を一旦Gitに預けます ---
+:: .gitignoreを無視してdistフォルダを強制追加
 git add -f dist/
 git add .
-
-echo --- 4. コミットして送信します ---
 set datetime=%date% %time%
-git commit -m "forced update !datetime!"
+git commit -m "pre-rebase update !datetime!"
+
+echo --- 3. GitHub側の変更を取り込んで合体させます ---
+:: これでREADMEの編集分などがローカルに反映されます
+git pull origin master --rebase
+
+echo --- 4. 最後にGitHubへ送りつけます ---
 git push origin master
 
 echo.
-echo --- 完了！GitHubのページを確認してください ---
+echo ---完了！GitHubの dist/assets を確認してください ---
 pause
