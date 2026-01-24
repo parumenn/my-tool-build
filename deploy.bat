1ce94aa4d343ac03323f15204ea193678e914d54
parumen <thonglo02cocoa@gmail.com>
2026-01-24 19:22:57 +0900

    forced update 2026/01/24 19:22:57.01

--- deploy.bat
+++ deploy.bat
@@ -4,18 +4,17 @@ setlocal enabledelayedexpansion
 echo --- 1. ビルドを開始します ---
 call npm run build
 
-echo --- 2. GitHubへの送信準備 (assetsを強制追加) ---
-:: .gitignoreを無視してdistフォルダを無理やり追加します
+echo --- 2. Gitの無視設定を上書きして追加します ---
+:: .gitignoreを無視してdistフォルダを強制的にステージングします
 git add -f dist/
-
-:: その他の変更も追加
+:: その他の変更も一括で追加
 git add .
 
-echo --- 3. コミットして送信します ---
+echo --- 3. GitHubへ送信します ---
 set datetime=%date% %time%
-git commit -m "auto update !datetime!"
+git commit -m "forced update !datetime!"
 git push origin master
 
 echo.
-echo --- 完了！GitHubに assets フォルダがあるか確認してください ---
+echo --- 完了！GitHubのページをリロードして assets があるか見てください ---
 pause
