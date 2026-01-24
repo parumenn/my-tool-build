52b2e1127da775de3e00d9ef1b97790d4ec7fe96
parumen <thonglo02cocoa@gmail.com>
2026-01-24 19:23:37 +0900

    forced update 2026/01/24 19:23:36.86

--- deploy.bat
+++ deploy.bat
@@ -4,17 +4,19 @@ setlocal enabledelayedexpansion
 echo --- 1. ビルドを開始します ---
 call npm run build
 
-echo --- 2. Gitの無視設定を上書きして追加します ---
-:: .gitignoreを無視してdistフォルダを強制的にステージングします
+echo --- 2. GitHub側の変更（READMEなど）を取り込みます ---
+:: 先にリモートの変更を取得して合体させます
+git pull origin master --rebase
+
+echo --- 3. Gitの無視設定を上書きして追加します ---
 git add -f dist/
-:: その他の変更も一括で追加
 git add .
 
-echo --- 3. GitHubへ送信します ---
+echo --- 4. コミットして送信します ---
 set datetime=%date% %time%
 git commit -m "forced update !datetime!"
 git push origin master
 
 echo.
-echo --- 完了！GitHubのページをリロードして assets があるか見てください ---
+echo --- 完了！GitHubのページを確認してください ---
 pause
